a18b2753d316c2e73ac9eae9bde8178c1c492069
Faizan Ali <devfaizi435@gmail.com>
2022-09-23 22:20:18 -0700

    string length

---
+++ string length/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.length;
+    return c;
+};
+var a = "Hello i am a man";
+console.log(Hello(a));
---
+++ string length/script.ts
@@ -0,0 +1,7 @@
+const Hello =(b:string):number =>{
+    let c:number=b.length;
+    return c
+
+}
+let a:string="Hello i am a man"
+console.log( Hello(a))
