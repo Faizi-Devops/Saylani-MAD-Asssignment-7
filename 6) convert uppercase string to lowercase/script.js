5b6fe15810faf8664fba3f95d927d92ee0e6dcbe
Faizan Ali <devfaizi435@gmail.com>
2022-09-24 22:35:07 -0700

    convert uppercase string to lowercase

---
+++ 6) convert uppercase string to lowercase/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.toLowerCase();
+    return c;
+};
+var a = "HELLO I AM A MAN MAN";
+console.log(Hello(a));
---
+++ 6) convert uppercase string to lowercase/script.ts
@@ -0,0 +1,7 @@
+let Hello = (b:string):string =>{
+    let c:string=b.toLowerCase();
+    return c;
+
+}
+let a:string="HELLO I AM A MAN MAN";
+console.log( Hello(a)  );
