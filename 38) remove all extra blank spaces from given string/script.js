fab83d82da5ff2ad603fa38bd133ba8f7d50439a
Faizan Ali <devfaizi435@gmail.com>
2022-09-25 23:41:39 -0700

    remove all extra blank spaces from given string

---
+++ 38) remove all extra blank spaces from given string/script.js
@@ -0,0 +1,7 @@
+var Hello = function (b) {
+    var c = b.split(" ");
+    var d = c.join("");
+    console.log(d);
+};
+var a = "Hello i am a man";
+Hello(a);
---
+++ 38) remove all extra blank spaces from given string/script.ts
@@ -0,0 +1,12 @@
+const Hello = (b:string) =>{
+
+    let c:string[]=b.split(" ");
+   let d:string=c.join("");
+   console.log(d)
+
+
+
+
+}
+let a:string="Hello i am a man"
+Hello(a);
