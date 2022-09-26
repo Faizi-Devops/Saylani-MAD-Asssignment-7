f81742751ae18c4d6b9dfd52252841182dd97446
Faizan Ali <devfaizi435@gmail.com>
2022-09-25 22:17:16 -0700

    concat two strings

---
+++ 3) concatenate two strings/script.js
@@ -0,0 +1,6 @@
+var Hello = function (c, d) {
+    return c.concat(d);
+};
+var a = "Hello this is a man ";
+var b = "I am a working on languuage";
+console.log(Hello(a, b));
---
+++ 3) concatenate two strings/script.ts
@@ -0,0 +1,8 @@
+var Hello = (c:string,d:string):string =>{
+    return c.concat(d);
+
+
+}
+var a:string="Hello this is a man ";
+var b:string="I am a working on languuage"
+console.log( Hello(a,b));
