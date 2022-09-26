cbb21a7552ed8e42f7258cb220212507dfbe7637
Faizan Ali <devfaizi435@gmail.com>
2022-09-26 00:58:54 -0700

    remove all occurrence of a word in given string

---
+++ 34) remove all occurrence of a word in given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.replace(/I/g, "");
+    console.log(c);
+};
+var a = "I love the Pakistan and I love the country";
+Hello(a);
---
+++ 34) remove all occurrence of a word in given string/script.ts
@@ -0,0 +1,9 @@
+const Hello = (b:string) =>{
+    let c:string=b.replace(/I/g,"");
+    console.log(c)
+
+
+
+}
+let a:string="I love the Pakistan and I love the country";
+Hello(a);
