c0fdbfa9ea30438f18e096f13a890d138c8bc9f0
Faizan Ali <devfaizi435@gmail.com>
2022-09-26 00:25:41 -0700

    trim leading white space characters from given string

---
+++ 35) trim leading white space characters from given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.trimStart();
+    console.log(c.length);
+};
+var a = "          Hello i am a man";
+Hello(a);
---
+++ 35) trim leading white space characters from given string/script.ts
@@ -0,0 +1,10 @@
+const Hello = (b:string) =>{
+    let c:string=b.trimStart();
+    console.log(c.length)
+
+
+
+}
+let a:string="          Hello i am a man";
+
+Hello(a);
