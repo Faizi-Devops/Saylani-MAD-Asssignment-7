2ef2cffc90ea40c3fb8a8b7575be3ceaeddd28c6
Faizan Ali <devfaizi435@gmail.com>
2022-09-25 23:51:43 -0700

    trim both leading and trailing white space characters from given string

---
+++ 37) trim both leading and trailing white space characters from given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.trim();
+    console.log(c);
+};
+var a = "         Hello i am a man      ";
+Hello(a);
---
+++ 37) trim both leading and trailing white space characters from given string/script.ts
@@ -0,0 +1,8 @@
+let Hello = (b:string) =>{
+    let c:string=b.trim();
+    console.log(c);
+
+}
+let a:string="         Hello i am a man      ";
+
+Hello(a);
