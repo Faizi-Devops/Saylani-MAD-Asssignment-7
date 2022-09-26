7faf68dc6a95d2bacf2448279820262acba47e80
Faizan Ali <devfaizi435@gmail.com>
2022-09-26 02:50:22 -0700

    print day of week name using switch case

---
+++ 1)print day of week name using switch case/script.js
@@ -0,0 +1,35 @@
+var a = 1;
+var day = "";
+switch (a) {
+    case 1:
+        day = "Monday";
+        console.log(day);
+        break;
+    case 2:
+        day = "Tuesday";
+        console.log(day);
+        break;
+    case 3:
+        day = "Wednesday";
+        console.log(day);
+        break;
+    case 4:
+        day = "Thursday";
+        console.log(day);
+        break;
+    case 5:
+        day = "Friday";
+        console.log(day);
+        break;
+    case 6:
+        day = "Saturday";
+        console.log(day);
+        break;
+    case 7:
+        day = "Sunday";
+        console.log(day);
+        break;
+    default:
+        console.log("Nothing Found");
+        break;
+}
---
+++ 1)print day of week name using switch case/script.ts
@@ -0,0 +1,43 @@
+let a:number=1;
+let day="";
+switch (a) {
+    case 1:
+        day="Monday";
+        console.log(day)
+        
+        break;
+        case 2:
+        day="Tuesday";
+        console.log(day)
+        
+        break;
+        case 3:
+        day="Wednesday";
+        console.log(day)
+        
+        break;
+        case 4:
+        day="Thursday";
+        console.log(day)
+        
+        break;
+        case 5:
+        day="Friday";
+        console.log(day)
+        
+        break;
+        case 6:
+        day="Saturday";
+        console.log(day)
+        
+        break;
+        case 7:
+        day="Sunday";
+        console.log(day)
+        
+        break;
+
+    default:
+        console.log("Nothing Found")
+        break;
+}
