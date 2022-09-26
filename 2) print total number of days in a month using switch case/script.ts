18e38b8b0b4d9c7640b70b6fc7105f9604026ab9
Faizan Ali <devfaizi435@gmail.com>
2022-09-26 03:02:51 -0700

    case 1:

---
+++ 2) print total number of days in a month using switch case/script.ts
@@ -0,0 +1,55 @@
+var month:number=1;
+switch (month) {
+    case 1:
+        console.log(`The month is January and days are 31`);
+        
+        break;
+        case 2:
+        console.log(`The month is Februaray and days are 28 and in leap year 29`);
+        
+        break;
+        case 3:
+        console.log(`The month is March and days are 31`);
+        
+        break;
+        case 4:
+        console.log(`The month is April and days are 30`);
+        
+        break;
+        case 5:
+        console.log(`The month is May and days are 31`);
+        
+        break;
+        case 6:
+        console.log(`The month is June and days are 30`);
+        
+        break;
+        case 7:
+        console.log(`The month is July and days are 31`);
+        
+        break;
+        case 8:
+        console.log(`The month is August and days are 31`);
+        
+        break;
+        case 9:
+        console.log(`The month is September and days are 30`);
+        
+        break;
+        case 10:
+        console.log(`The month is October and days are 31`);
+        
+        break;
+        case 11:
+        console.log(`The month is November and days are 30`);
+        
+        break;
+        case 12:
+        console.log(`The month is December and days are 31`);
+        
+        break;
+
+    default:
+        console.log("Nothing found")
+        break;
+}
