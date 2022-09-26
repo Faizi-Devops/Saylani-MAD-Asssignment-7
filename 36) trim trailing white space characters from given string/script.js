681313bd78698a4119bbfed0a3c08c40fdbedfde
Faizan Ali <devfaizi435@gmail.com>
2022-09-26 00:19:32 -0700

    trim trailing white space characters from given string

---
+++ 36) trim trailing white space characters from given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.trimEnd();
+    console.log(c);
+};
+var a = "Hello i am a man         ";
+Hello(a);
---
+++ 36) trim trailing white space characters from given string/script.ts
@@ -0,0 +1,7 @@
+const Hello = (b:string) =>{
+    let c:string=b.trimEnd();
+    console.log(c);
+
+}
+let a:string="Hello i am a man         ";
+Hello(a);
