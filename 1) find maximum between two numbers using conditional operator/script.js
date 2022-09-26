9d55c8183b83dc58c42809d87496923959510141
Faizan Ali <devfaizi435@gmail.com>
2022-09-25 22:18:48 -0700

    maximum between two numbers using conditional operator

---
+++ 1) find maximum between two numbers using conditional operator/script.js
@@ -0,0 +1,6 @@
+var Hello = function (c, d) {
+    (c >= d) ? console.log("".concat(c, " is greater then the ").concat(d)) : console.log("".concat(d, " is grater then ").concat(c));
+};
+var a = 46;
+var b = 56;
+Hello(a, b);
---
+++ 1) find maximum between two numbers using conditional operator/script.ts
@@ -0,0 +1,7 @@
+let Hello = (c:number,d:number) =>{
+    (c>=d)?console.log(`${c} is greater then the ${d}`):console.log(`${d} is grater then ${c}`)
+
+}
+let a:number=46;
+let b:number=56;
+Hello(a,b)
