41171c0c5a5cc4b19fe3f3be18ad263024ed710f
Faizan Ali <devfaizi435@gmail.com>
2022-09-28 00:08:33 -0700

    find first occurrence of a word in a given string

---
+++ 28) find first occurrence of a word in a given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.indexOf("love");
+    return c;
+};
+var a = "I love the Minahil and she is my love";
+console.log(Hello(a));
---
+++ 28) find first occurrence of a word in a given string/script.ts
@@ -0,0 +1,10 @@
+const Hello = (b:string):number =>{
+    let c:number=b.indexOf("love");
+    return c;
+    
+
+
+
+}
+let a:string="I love the Minahil and she is my love";
+console.log( Hello(a));
