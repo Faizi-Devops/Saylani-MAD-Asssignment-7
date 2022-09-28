cc5c1443cd3b39fc52e79d729f61d129d3131f9c
Faizan Ali <devfaizi435@gmail.com>
2022-09-28 00:01:41 -0700

    a number is even or odd using conditional operator

---
+++ 3) a number is even or odd using conditional operator/script.js
@@ -0,0 +1,5 @@
+var Hello = function (b) {
+    (b % 2 == 0) ? console.log("".concat(b, " is even number")) : console.log("".concat(b, " is odd number"));
+};
+var a = 11;
+Hello(a);
---
+++ 3) a number is even or odd using conditional operator/script.ts
@@ -0,0 +1,9 @@
+const Hello = (b:number) =>{
+    (b%2==0)?console.log(`${b} is even number`):console.log(`${b} is odd number`);
+    
+
+
+
+}
+let a:number=11;
+Hello(a);
