3de1edf2449dcb4e64f52ed04dd2f4379f51841b
Faizan Ali <devfaizi435@gmail.com>
2022-09-29 01:36:03 -0700

    find last occurrence of a word in a given string

---
+++ 29) find last occurrence of a word in a given string/script.js
@@ -0,0 +1,6 @@
+var Hello = function (b) {
+    var c = b.lastIndexOf("love");
+    return c;
+};
+var a = "I love programming. I love Codeforwin.";
+console.log(Hello(a));
---
+++ 29) find last occurrence of a word in a given string/script.ts
@@ -0,0 +1,9 @@
+const Hello = (b:string):number =>{
+    let c:number=b.lastIndexOf("love");
+    return c
+    
+
+
+}
+let a:string="I love programming. I love Codeforwin.";
+console.log( Hello(a) );
